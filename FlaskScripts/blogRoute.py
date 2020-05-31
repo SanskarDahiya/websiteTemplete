a0d7a9db5641433d90c69b14be3e8be1e3c6e496
SanskarDahiya <sanskardahiya98@gmail.com>
2020-05-31 18:04:43 +0530

    to blog only

--- FlaskScripts/blogRoute.py
+++ FlaskScripts/blogRoute.py
@@ -1,99 +1,31 @@
 from flask import *     # pylint: disable=unused-wildcard-import
 from FlaskScripts.database.database import blog as blogDB
 
-blogs = Blueprint('blogRoute', __name__, url_prefix='/blog',
-                  template_folder='../templates')
+blogs = Blueprint('blogRoute', __name__, url_prefix='/blog')
 
-myblog = [
-    {
-        "_id": "0",
-        "image": "images/image_1.jpg",
-        "date": [26, 'June', 2019],
-        "title":"Skills To Develop Your Child Memory",
-        "desc":"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-        "uploadedBy":{
-            "name": "Admin",
-            "_id": "123",
-        },
-        "comments": ['12', '34'],
-        "category":['A', "S", "D"]
-    }, {
-        "_id": "1",
-        "image": "images/image_1.jpg",
-        "date": [26, 'June', 2019],
-        "title":"Skills To Develop Your Child Memory",
-        "desc":"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-        "uploadedBy":{
-            "name": "Admin",
-            "_id": "123",
-        },
-        "comments": ['12', '34'],
-        "category":['A', "S", "D"]
-    }, {
-        "_id": "2",
-        "image": "images/image_1.jpg",
-        "date": [26, 'June', 2019],
-        "title":"Skills To Develop Your Child Memory",
-        "desc":"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-        "uploadedBy":{
-            "name": "Admin",
-            "_id": "123",
-        },
-        "comments": ['12', '34'],
-        "category":['A', "S", "D"]
-    }, {
-        "_id": "3",
-        "image": "images/image_1.jpg",
-        "date": [26, 'June', 2019],
-        "title":"Skills To Develop Your Child Memory",
-        "desc":"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-        "uploadedBy":{
-            "name": "Admin",
-            "_id": "123",
-        },
-        "comments": ['12', '34'],
-        "category":['A', "S", "D"]
-    }, {
-        "_id": "4",
-        "image": "images/image_1.jpg",
-        "date": [26, 'June', 2019],
-        "title":"Skills To Develop Your Child Memory",
-        "desc":"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-        "uploadedBy":{
-            "name": "Admin",
-            "_id": "123",
-        },
-        "comments": ['12', '34'],
-        "category":['A', "S", "D"]
-    },
-]
-@blogs.route("/")
+z = {
+    'hi': "okay"
+}
+
+
+@blogs.route("/", methods=['GET', 'POST'])
 def showBlogData():
-    return make_response(render_template("blog.html", blogs=myblog))
-
-    # try:
-    #     allblogs = blogDB.findblogs()
-    #     return render_template("blog.html",data = allblogs)
-    #     # return 'Blogs are>> '+str(allblogs)
-    # except:
-    #     return '/blog: Somthing Wrong'
+    try:
+        response = make_response(z)
+        return response
+    except:
+        return '/blog: Somthing Wrong'
 
 
 @blogs.route("/singleblog", methods=['GET', 'POST'])
 def singleblog():
-    cstmId = int(request.args.get("id")) or 0
-    return make_response(render_template('blog-single.html', blog=myblog[cstmId]))
+    cstmId = int(request.args.get("id") or 0)
+    return make_response('single blog')
 
 
 @blogs.route("/addtoblog", methods=['GET', 'POST'])
 def addBlogData():
     try:
-        if request.method == 'POST':
-            data = dict(request.form)
-            data['name'] = 'username'
-            data['email'] = 'Email'
-            data['date'] = 'Data-2020'
-            data = blogDB.addblog(data)
-            return redirect('/blog')
+        return 'okay'
     except:
         return '/blog: Somthing Wrong'
--- Index.py
+++ Index.py
@@ -1,90 +1,17 @@
 from flask import *     # pylint: disable=unused-wildcard-import
-from FlaskScripts.usersRoute import users
 from FlaskScripts.blogRoute import blogs
-from FlaskScripts.database.database import user as userDB  # database
-
 import os
-import csv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-
-# all /user will redirect here
-app.register_blueprint(users)
 app.register_blueprint(blogs)
 
-
-@app.route('/about')
-def about():
-    return make_response(render_template("about.html"))
-
-
-@app.route('/blogs')
-def singleBlog():
-    return make_response(render_template("blog-single.html"))
-
-
-@app.route("/courses")
-def courses():
-    return make_response(render_template("courses.html"))
-
-
-@app.route("/staff")
-def staff():
-    return make_response(render_template("teacher.html"))
-
-
-@app.route("/contact")
-def contact():
-    return make_response(render_template("contact.html"))
-
+CORS(app)
 
 @app.route("/")
 def index():
-    data = [
-        {
-            'imageUrl': "images/bg_1.jpg",
-            "title": "ELearning",
-            "desc": " Computer Science And Engineering",
-            "button": [{
-                "title": "2020",
-                "link": "/"
-            }]
-        }, {
-            'imageUrl': "images/bg_1.jpg",
-            "title": "ELearning",
-            "desc": " Computer Science And Engineering",
-            "button": [{
-                "title": "2020",
-                "link": "/"
-            }]
-        }, {
-            'imageUrl': "images/bg_1.jpg",
-            "title": "ELearning",
-            "desc": " Computer Science And Engineering",
-            "button": [{
-                "title": "2020",
-                "link": "/"
-            }]
-        }, {
-            'imageUrl': "images/bg_1.jpg",
-            "title": "ELearning",
-            "desc": " Computer Science And Engineering",
-            "button": [{
-                "title": "2020",
-                "link": "/"
-            }]
-        }, ]
-    resp = make_response(render_template("index.html", headingImages=data))
-    if(request.cookies.get('userid')):
-        userdata = userDB.finduser(
-            '_id', request.cookies.get('userid')) or False
-        print('>>>', userdata)
-        if(userdata):
-            resp = make_response(render_template(
-                "index.html", data=userdata[0]))
-            return resp
-    resp.set_cookie('userid', 'False', 0)
+    resp = make_response('THIS IS RUNNING')
     return resp
 
 
